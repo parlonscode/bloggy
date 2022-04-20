5f93cee0828a738ad59f2157e22bab5a41c3ad33
Honore Hounwanou <mercuryseries@gmail.com>
2022-04-20 16:50:20 -0400

    Redirect user to homepage if already logged in

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,6 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
+            // TODO: Add flash message
             return $this->redirectToRoute('app_home');
         }
 
