00da08acb5560d2ec1aab8b9df0a71f21b732894
Honore Hounwanou <mercuryseries@gmail.com>
2022-04-20 16:43:00 -0400

    Redirect user to homepage if already logged in

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,9 +12,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
