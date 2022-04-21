d2a823e1002e4204d0bd2997454e9e0a851068cc
Honore Hounwanou <mercuryseries@gmail.com>
2022-04-21 17:01:39 -0400

    Add show action and link it from index

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +18,11 @@ class PostsController extends AbstractController
         $posts = $postRepository->findAll();
 
         return $this->render('posts/index.html.twig', compact('posts'));
+    }
+
+    #[Route('/posts/{slug}', name: 'app_posts_show')]
+    public function show(Post $post): Response
+    {
+        return $this->render('posts/show.html.twig', compact('post'));
     }
 }
