a42b024437a4cbac9ff6af58f99e6fa1a15f08cf
Honore Hounwanou <mercuryseries@gmail.com>
2022-09-08 17:16:54 -0400

    WIP

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,43 +7,44 @@ use App\Entity\Post;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher, private SluggerInterface $slugger) {}
+
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create('fr_FR');
-        dd($faker->name(), $faker->email(), $faker->realText());
+        $faker = Factory::create();
 
-        // $user = new User;
-        // $user->setName('Mercury Series');
-        // $user->setEmail('mercuryseries@gmail.com');
-        // // secret123
-        // $user->setPassword('$2y$13$CxN7gFXFLXu1v.nNP9paD.jvMiAaPx318BwOoFapnYYpvmbJv5l9e');
-        // $manager->persist($user);
+        $user = new User;
+        $user->setName('Mercury Series');
+        $user->setEmail('mercuryseries@gmail.com');
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'secret123'));
+        $manager->persist($user);
         
-        // $admin = new User;
-        // $admin->setName('Mister Admin');
-        // $admin->setEmail('admin@bloggy.wip');
-        // $admin->setRoles(['ROLE_ADMIN']);
-        // // secret123
-        // $admin->setPassword('$2y$13$dfAdM52nYyvgbmQjShEjFuOhphhWmx8hL/rwo4QbinMpSBzckfio.');
-        // $manager->persist($admin);
+        $admin = new User;
+        $admin->setName('Mister Admin');
+        $admin->setEmail('admin@bloggy.wip');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'secret123'));
+        $manager->persist($admin);
         
-        // for ($i = 1; $i <= 10; $i++) {
-        //     $post = new Post;
-        //     $post->setTitle('Post '.$i);
-        //     $post->setSlug('post-'.$i);
-        //     $post->setBody('El charabia here just to test.');
-        //     $post->setPublishedAt(
-        //         mt_rand(1, 10) >= 5
-        //         ? new \DateTimeImmutable(sprintf('-%d days', mt_rand(10, 50)))
-        //         : null
-        //     );
-        //     $post->setAuthor(mt_rand(1, 10) >= 5 ? $user : $admin);
-        //     $manager->persist($post);
-        // }
+        for ($i = 1; $i <= 10; $i++) {
+            $post = new Post;
+            $post->setTitle($title = $faker->unique()->sentence());
+            $post->setSlug($this->slugger->slug(mb_strtolower($title)));
+            $post->setBody($faker->paragraph(10));
+            $post->setPublishedAt(
+                $faker->boolean(75)
+                ? \DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-50 days', '-10 days'))
+                : null
+            );
+            $post->setAuthor($faker->boolean(50) ? $user : $admin);
+            $manager->persist($post);
+        }
 
-        // $manager->flush();
+        $manager->flush();
     }
 }
