ce294479e5019e604d0752d8292a6e9a0db9492c
Honore Hounwanou <mercuryseries@gmail.com>
2022-12-08 09:54:51 -0500

    Remove sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
